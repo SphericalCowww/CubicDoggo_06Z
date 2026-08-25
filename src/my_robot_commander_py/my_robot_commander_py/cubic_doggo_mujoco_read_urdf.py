7174adfe94bc55b34ba30339c883c7a77415b307
SphericalCowww <tinglin194@gmail.com>
2026-08-26 00:48:32 +0200

    - reordering the mujoco urdf parser such that <keyframe> for joint position are applied after urdf inclusion in the xml file

--- src/my_robot_commander_py/my_robot_commander_py/cubic_doggo_mujoco_read_urdf.py
+++ src/my_robot_commander_py/my_robot_commander_py/cubic_doggo_mujoco_read_urdf.py
@@ -1,27 +1,34 @@
 from ament_index_python.packages import get_package_share_directory
-import os
-import xacro
-import mujoco
-import mujoco.viewer
-##########################################################################################
+import os, re, tempfile
+import mujoco, mujoco.viewer
+#############################################################################################################################
 def main():
     pkg_share_path = get_package_share_directory('my_robot_description')
-    xacro_path     = os.path.join(pkg_share_path, 'urdf', 'cubic_doggo.mujoco.xacro')
-    world_path     = os.path.join(pkg_share_path, 'world', 'scene.xml')
+    urdf_path      = os.path.join(pkg_share_path, 'urdf', 'cubic_doggo.mujoco.urdf')
+    mjcf_path      = os.path.join(pkg_share_path, 'urdf', 'cubic_doggo.mujoco.xml')
 
-    xacro_file = xacro.process_file(xacro_path)
-    urdf_xml = xacro_file.toxml()
-    urdf_xml = urdf_xml.replace('package://my_robot_description', pkg_share_path)
+    with open(urdf_path, 'r') as fileObj:
+        urdf_raw = fileObj.read()
+    urdf_content = urdf_raw.replace('package://my_robot_description', pkg_share_path)
+    urdf_content = urdf_content.replace('</robot>', '<mujoco><compiler discardvisual="false"/></mujoco></robot>')
+    urdf_content_temp = mujoco.MjModel.from_xml_string(urdf_content)
+    with tempfile.NamedTemporaryFile(mode='w+', suffix='.xml') as tempfileObj:
+        mujoco.mj_saveLastXML(tempfileObj.name, urdf_content_temp)
+        tempfileObj.seek(0)
+        urdf_content = tempfileObj.read()
+    robot_assets = re.search(r'<asset>(.*?)</asset>',         urdf_content, re.DOTALL).group(1)
+    robot_bodies = re.search(r'<worldbody>(.*?)</worldbody>', urdf_content, re.DOTALL).group(1)
 
-    robot_spec = mujoco.MjSpec.from_string(urdf_xml)
-    world_spec = mujoco.MjSpec.from_file(world_path)
-    frame = world_spec.worldbody.add_frame()
-    frame.pos = [0, 0, 0.2]  # Optional: lift robot slightly above floor
-    frame.attach_body(robot_spec.worldbody.bodies[0], "", "")
+    with open(mjcf_path, 'r') as fileObj:
+        mjcf_content = fileObj.read()
+    mjcf_content = mjcf_content.replace('</asset>', robot_assets + '\n    </asset>')
+    mjcf_content = mjcf_content.replace('<include file="cubic_doggo.mujoco.urdf"/>',robot_bodies)
 
-    model = world_spec.compile()
+    model = mujoco.MjModel.from_xml_string(mjcf_content)
     data = mujoco.MjData(model)
+    if model.nkey > 0:
+        mujoco.mj_resetDataKeyframe(model, data, 0)
     mujoco.viewer.launch(model, data)
-##########################################################################################
+#############################################################################################################################
 if __name__ == '__main__': main()
 
