c239636fba9070974987ed80b3289e5424ad5e1b
SphericalCowww <tinglin194@gmail.com>
2026-09-02 20:33:03 +0200

    - adding cubic_leg1 for debugging

--- src/my_robot_commander_py/my_robot_commander_py/cubic_doggo_mujoco_read_urdf.py
+++ src/my_robot_commander_py/my_robot_commander_py/cubic_doggo_mujoco_read_urdf.py
@@ -5,8 +5,9 @@ import mujoco, mujoco.viewer
 #############################################################################################################################
 def main():
     pkg_share_path = get_package_share_directory('my_robot_description')
-    xacro_path     = os.path.join(pkg_share_path, 'urdf', 'cubic_doggo.urdf.xacro')
-    mjcf_path      = os.path.join(pkg_share_path, 'urdf', 'cubic_doggo.mujoco.xml')
+    xacro_path     = os.path.join(pkg_share_path, 'urdf', 'cubic_leg1.urdf.xacro')
+    mjcf_path      = os.path.join(pkg_share_path, 'urdf', 'cubic_leg1.mujoco.xml')
+    usdf_file      =                                      'cubic_leg1.mujoco.urdf'
 
     xacro_raw = xacro.process_file(xacro_path)
     urdf_raw  = xacro_raw.toxml()
@@ -23,10 +24,28 @@ def main():
     with open(mjcf_path, 'r') as fileObj:
         mjcf_content = fileObj.read()
     mjcf_content = mjcf_content.replace('</asset>', robot_assets + '\n    </asset>')
-    mjcf_content = mjcf_content.replace('<include file="cubic_doggo.mujoco.urdf"/>',robot_bodies)
+    mjcf_content = mjcf_content.replace('<include file=\"'+usdf_file+'\"/>', robot_bodies)
+
+    '''
+    stripped_xml = mjcf_content.split('<contact>')[0] + '</mujoco>'
+    debug_model = mujoco.MjModel.from_xml_string(stripped_xml)
+    print("mujoco body names:")
+    for bodyIdx in range(debug_model.nbody):
+        print("  "+str(bodyIdx)+": "+debug_model.body(bodyIdx).name)
+    '''
 
     model = mujoco.MjModel.from_xml_string(mjcf_content)
     data = mujoco.MjData(model)
+
+    '''
+    print("mujoco joint names:")
+    for jointIdx in range(model.njnt):
+        jnt_name = model.joint(jointIdx).name
+        jnt_type = model.jnt_type[jointIdx]
+        qpos_idx = model.jnt_qposadr[jointIdx]
+        print("  qpos["+str(jointIdx)+"]: "+jnt_name)
+    '''
+
     if model.nkey > 0:
         mujoco.mj_resetDataKeyframe(model, data, 0)
     mujoco.viewer.launch(model, data)
